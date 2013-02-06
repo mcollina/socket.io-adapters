55a4fa385959eb0ffbbb272bb153d14723067edc
Matteo Collina <matteo.collina@gmail.com>
2013-02-06 18:46:46 +0000

    Written the first test for the original adapter.
    References #1.

--- test/behave_like_an_adapter.js
+++ test/behave_like_an_adapter.js
@@ -1,4 +1,26 @@
 
 module.exports = function() {
 
+  var stubSocket = function(namespace, id, fn) {
+    var socket = {
+      packet: fn
+    }
+    namespace.connected = namespace.connected || {};
+    namespace.connected[id] = socket;
+
+    return socket;
+  };
+
+  it("should forward broadcast", function(done) {
+    var packet = "this is a packet";
+
+    stubSocket(this.namespace, 42, function(result) {
+      expect(result).to.equal(packet);
+      done();
+    });
+
+    this.instance.add(42, "room");
+
+    this.instance.broadcast(packet, {});
+  });
 };
